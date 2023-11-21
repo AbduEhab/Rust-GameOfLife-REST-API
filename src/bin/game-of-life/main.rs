61537cda11371a5f960287f410177ecdfc622d5f
AbduEhab <abdu.ehab0@gmail.com>
2023-11-21 20:53:25 +0100

    miner changes

--- src/bin/game-of-life/main.rs
+++ src/bin/game-of-life/main.rs
@@ -5,6 +5,8 @@ use std::f32::consts::PI;
 use bevy::{pbr::CascadeShadowConfigBuilder, prelude::*};
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
+use rand::prelude::*;
+
 use minreq;
 
 fn main() {
@@ -12,7 +14,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Startup, test_call)
+        .add_systems(Update, process_cells)
         .run();
 }
 
@@ -21,12 +23,29 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+    // spawn 32*32*32 cubes
+    for x in 0..32 {
+        for y in 0..32 {
+            for z in 0..32 {
+                commands
+                    .spawn(PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                        material: materials.add(Color::rgb(0.0, 0.0, 0.0).into()),
+                        transform: Transform::from_translation(Vec3::new(
+                            x as f32 - 16.0,
+                            y as f32 - 16.0,
+                            z as f32 - 16.0,
+                        )),
+                        ..Default::default()
+                    })
+                    .insert(Cell {
+                        alive: rand::thread_rng().gen_bool(0.5),
+                    })
+                    .insert(id(x * 32 * 32 + y * 32 + z));
+            }
+        }
+    }
+
     // Light
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -63,26 +82,66 @@ fn setup(
     ));
 }
 
-// create a bevy component
 #[derive(Component)]
-struct GOL{
-    cells: [Cell; 64 * 64]
-}
+struct id(usize);
 
-
+#[derive(Component)]
 struct Cell {
     alive: bool,
-    neighbors: u8,
 }
 
-fn test_call() {
-    let name = "world";
+fn process_cells(mut cells: Query<(&mut Cell, &id, &mut Handle<StandardMaterial>)>) {
+    let i = 0;
 
-    let res = minreq::get(format!("http://127.0.0.1:3000/api/hello?name={}", name))
-        .send()
-        .unwrap();
+    for (mut cell, cell_id, material) in cells.iter_mut() {
+        // find the cell's neighbors (8)
+        let mut neighbors = 0;
 
-    // form a string from the response's body
-    let body = res.as_str().unwrap();
-    println!("{}", body);
+        let cell_id = cell_id.0;
+
+        let (x, y, z) = (cell_id / (32 * 32), (cell_id / 32) % 32, cell_id % 32);
+
+        // get the state of the 8 neighbors
+        for i in -1..2 {
+            for j in -1..2 {
+                for k in -1..2 {
+                    if i == 0 && j == 0 && k == 0 {
+                        continue;
+                    }
+
+                    let (x, y, z) = (
+                        (x as i32 + i) as u32,
+                        (y as i32 + j) as u32,
+                        (z as i32 + k) as u32,
+                    );
+
+                    let neighbor_id = x * 32 * 32 + y * 32 + z;
+
+                    if neighbor_id >= 32 * 32 * 32 {
+                        continue;
+                    }
+
+                    let neighbor = cells.get_mut(id(neighbor_id as usize));
+
+                    if neighbor.is_ok() {
+                        if neighbor.unwrap().alive {
+                            neighbors += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // let name = "world";
+
+    // let res = minreq::get(format!("http://127.0.0.1:3000/api/hello?name={}", name))
+    //     .send()
+    //     .unwrap();
+
+    // // form a string from the response's body
+    // let body = res.as_str().unwrap();
+    // println!("{}", body);
 }
+
+// get all
